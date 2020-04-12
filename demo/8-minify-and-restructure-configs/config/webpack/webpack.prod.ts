65e051cac9decf85ee3a00d3696e7c8720414303
seognil LC <seognil@gmail.com>
2020-04-12 16:20:05 +0800

    refactor(demo): rename 8

--- demo/8-minify-and-restructure-configs/config/webpack/webpack.prod.ts
+++ demo/8-minify-and-restructure-configs/config/webpack/webpack.prod.ts
@@ -2,6 +2,6 @@ import { config, extendConfigs } from './webpack.base';
 
 config.mode('production');
 
-extendConfigs(['./optional-configs/clean-dist.ts']);
+extendConfigs(['./optional-configs/clean-dist.ts', './optional-configs/terser.ts']);
 
 export default config.toConfig();
