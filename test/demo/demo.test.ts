31a74cfe104bc646688fd93a08e1445389e0c6a0
seognil LC <seognil@gmail.com>
2020-04-16 17:04:29 +0800

    test: update demo test

--- test/demo/demo.test.ts
+++ test/demo/demo.test.ts
@@ -2,9 +2,16 @@ import path from 'path';
 import glob from 'glob';
 import { execSync } from 'child_process';
 
-const projs = glob.sync(path.resolve(__dirname, '../../demo/{0,1,2,3,4,5,6,7,8}-*'));
+const numMatch = (p: string) => p.match(/(?<=demo\/)\d+(?=[-])/);
 
-const commands = Object.values({
+const numOf = (p: string) => Number(numMatch(p)![0]);
+
+const projs = glob
+  .sync(path.resolve(__dirname, '../../demo/*'))
+  .filter((e) => numMatch(e) !== null)
+  .sort((a, b) => numOf(a) - numOf(b));
+
+const commands = Object.entries({
   //
   0: [`yarn build; yarn test`],
   1: [`yarn build; yarn test`],
@@ -16,13 +23,18 @@ const commands = Object.values({
   7: [`yarn build; yarn test`, `yarn build:dev; yarn test`],
   8: [`yarn build; yarn test`, `yarn build:dev; yarn test`],
   9: [`yarn build; yarn test`, `yarn build:dev; yarn test`],
-}).map((cmds, i) => cmds.map((e) => e.replace(/^/, `cd ${projs[i]}; yarn; `)));
+  10: [`yarn build; yarn test`, `yarn build:dev; yarn test`, `yarn build:babel; yarn test:babel`],
+  11: [`yarn build; yarn test`, `yarn build:dev; yarn test`, `yarn build:babel; yarn test:babel`],
+  12: [`yarn build; yarn test`, `yarn build:dev; yarn test`],
+}).map(([i, cmds]) => cmds.map((e) => e.replace(/^/, `cd ${projs[Number(i)]}; yarn; `)));
+
+jest.retryTimes(0);
 
 describe(`run build`, () => {
   commands.forEach((cmds) => {
     cmds.forEach((c) => {
       test(c, () => {
-        execSync(c);
+        execSync(c, { stdio: 'inherit' });
       });
     });
   });
