d65f3caff5c6898eff8fccefb0e0d6746b557193
seognil LC <seognil@gmail.com>
2020-04-22 15:48:51 +0800

    fix: rename varible (typo)

--- demo/7-babel-basic/config/webpack/configs/resolve.ts
+++ demo/7-babel-basic/config/webpack/configs/resolve.ts
@@ -1,5 +1,5 @@
 import { ConfigTweaker } from '../types';
 
-export const configEntry: ConfigTweaker = (config, resolver) => {
+export const resolveConfig: ConfigTweaker = (config, resolver) => {
   config.resolve.extensions.merge(['.ts', '.tsx', '.js', '.jsx']);
 };
