a5fe17c2b36e3fe81dc82cf53654be98c309b55a
seognil LC <seognil@gmail.com>
2020-05-10 18:04:44 +0800

    fix: typo

--- demo/18-webpack-output-filename/config/webpack/configs/watch.ts
+++ demo/18-webpack-output-filename/config/webpack/configs/watch.ts
@@ -1,6 +1,6 @@
 import { isWatch } from './../utils/env';
 import { ConfigTweaker } from '../utils/types';
 
-export const loadScript: ConfigTweaker = (config, resolver) => {
+export const setWatch: ConfigTweaker = (config, resolver) => {
   config.watch(isWatch);
 };
